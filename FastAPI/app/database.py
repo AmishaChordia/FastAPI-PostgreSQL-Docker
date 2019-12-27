6378ca31022a23c54b1830ceb1cba30ea3b1d3e4
Amisha Chordia <achordia@deloitte.com>
2019-12-27 22:09:28 +0530

    Docker file with sqlite

--- FastAPI/app/database.py
+++ FastAPI/app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://amishachordia:9969612836@localhost/nudges"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://amishachordia:9969612836@localhost/nudges"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
