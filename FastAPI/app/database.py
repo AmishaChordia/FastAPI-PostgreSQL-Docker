460c40e66df03ea36ad445b09ba6085530d4d5c6
israelcode <31614943+israelcode@users.noreply.github.com>
2023-01-03 15:40:50 +0300

    Update database.py

--- FastAPI/app/database.py
+++ FastAPI/app/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/nudges"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:root@db:5432/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
